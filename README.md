b718c185e210b911fe11fc473eaf91297fd80104
brickwall <sk292207@gmail.com>
2023-07-24 13:24:27 +0530

    Add pomodoro timer

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# scripts
+pomodoro:
+ dependencies: install send-notify 
+ pop up remains untill it is closed
+ if you want more control, read docs of send-notify
---
+++ pomodoro.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo 'Enter time in seconds'
+read time
+
+echo 'Enter the message'
+read string
+
+sleep $time && notify-send --expire-time=0 $string &
