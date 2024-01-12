fe2e09aadbb0fdc1a687279c3b6286779194ba7c
brickwall <sk292207@gmail.com>
2024-01-12 14:27:47 +0530

    Use prompt to ask if music is to be played after the timer ends

--- pomodoro/pomodoro.rb
+++ pomodoro/pomodoro.rb
@@ -1,3 +1,6 @@
+require 'tty-prompt'
+require_relative './songs.rb'
+
 default_message = 'Time is up'
 default_time = 3600
 
@@ -9,9 +12,11 @@ puts 'Enter the message'
 message = gets.chomp
 message = message.empty? ? default_message : message
 
-songs = {:sooraj_dooba_hai => 'gZL0pXq8c2s', :makhna => '5D8wwE4_3sE'}
+song_prompt = prompt = TTY::Prompt.new
+song_prompt.yes? 'Do you want music to be played at the end of timer?'
 
 puts "Pomodoro timer for #{time}s activated"
 
-system("sleep #{time} && notify-send --expire-time=0 '#{message}' && mpv https://www.youtube.com/watch?v=#{songs.values.sample} &")
+system("sleep #{time} && notify-send --expire-time=0 '#{message}' &")
+system("sleep #{time} && mpv https://www.youtube.com/watch?v=#{songss.values.sample} &") if song_prompt
 
